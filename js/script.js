3e8b058933d6495c2177e543edce80d480b213ac
JameWeasley <101082867+JameWeasley@users.noreply.github.com>
2024-02-17 22:42:15 +0700

    s

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function showPopupMenu() {
+    let showPopup = document.getElementById('showPopup');
+
+    if (showPopup.style.display === 'none') {
+        showPopup.style.display = 'block';
+    } else {
+        showPopup.style.display = 'none';
+    }
+}
