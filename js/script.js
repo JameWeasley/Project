9035d19cc375f1e27b1a1e2ea477ee4cc0d8467e
RT <50390909+RT-XENON@users.noreply.github.com>
2024-02-19 21:16:29 +0700

    s

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+const { createApp , ref } = Vue
+
+createApp({
+    setup() {
+        const Name = ref("Test")
+        const count = ref(0)
+
+        const increase = function() {
+            count.value++
+        }
+
+        const decrease = function() {
+            count.value--
+        }
+
+        return {
+            Name,
+            count,
+            increase,
+            decrease
+        }
+    },
+    mounted() {
+        this.addAnimation()
+    },
+    methods: {
+        addAnimation() {
+            
+        }
+    }
+}).mount("body")
+
 function showPopupMenu() {
     let showPopup = document.getElementById('showPopup');
 
