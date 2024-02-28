e074842054abde0ebf18d9aa578cfe32410cb649
RT <50390909+RT-XENON@users.noreply.github.com>
2024-02-28 20:22:28 +0700

    upload

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require("express")
 const cors = require('cors');
 const path = require('path')
-
-// Front End
 const app = express()
+const multer = require("multer")
 
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 app.use(express.static('views'));
 app.use("/" , express.static(path.join(__dirname , 'views')))
 app.set('views', path.join(__dirname, 'views'));
-app.set("view engine" , "ejs")
+app.set('view engine', 'ejs');
 
 app.use(require("./routes/routes"))
 
+app.use((req , res , next) => {
+    return res.render("notfound")
+})
+
 app.listen(80 , () => {
-    console.log("Front End Running Port 80");
+    console.log("Running Port 80");
 })
--- routes/api.routes.js
+++ routes/api.routes.js
@@ -2,5 +2,16 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/" , (req, res ,next) => {
-    
+    return res.render("index")
 })
+
+router.get("/login" , (req, res ,next) => {
+    return res.render("login")
+})
+
+router.post("/login" , (req , res , next) => {
+    const { username , password } = req.body
+    // ดึงข้อมูลมา
+})
+
+module.exports = router
