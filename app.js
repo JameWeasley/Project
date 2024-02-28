a626e7cc78cd6055a7f6de0011031df1d4dcabd0
RT <50390909+RT-XENON@users.noreply.github.com>
2024-02-28 20:05:58 +0700

    upload

--- app.js
+++ app.js
@@ -5,29 +5,14 @@ const path = require('path')
 // Front End
 const app = express()
 
+
 app.use(express.static('views'));
 app.use("/" , express.static(path.join(__dirname , 'views')))
 app.set('views', path.join(__dirname, 'views'));
+app.set("view engine" , "ejs")
+
+app.use(require("./routes/routes"))
 
 app.listen(80 , () => {
     console.log("Front End Running Port 80");
-})
-
-
-// Backend
-
-const app_backend = express()
-
-app_backend.use(cors());
-
-app_backend.use(express.urlencoded({ extended: false }))
-app_backend.use(express.json())
-
-app_backend.get("/" , (req , res) => {
-    console.log("yes");
-    res.send("สวัสดีครับ")
-})
-
-app_backend.listen(500 , () => {
-    console.log("Backend Running Port 500");
 })
